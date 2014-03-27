e9e7563f50cb55a4d7a9d876e90c7d659fc4f5ef
Sam Rayner <sam@terracoding.com>
2014-03-27 16:09:54 +0000

    Add glossary tooltips, finish homage HTML, add images

--- helpers/theme_helpers.rb
+++ helpers/theme_helpers.rb
@@ -1,12 +1,12 @@
 module ThemeHelpers
-  def html_obfuscate(string, url=false)
-    if url
-      "%" + string.unpack("H2" * string.bytesize).join("%").upcase
-    else
-      encoded = []
-      string.each_char { |char| encoded << "&##{char[0].ord};" }
-      encoded.join
-    end
+  def entity_obfuscate(string)
+    encoded = []
+    string.each_char { |char| encoded << "&##{char[0].ord};" }
+    encoded.join
+  end
+
+  def percent_obfuscate(string)
+    "%" + string.unpack("H2" * string.bytesize).join("%").upcase
   end
 
   def image_collection(dir, type)
@@ -37,5 +37,15 @@ module ThemeHelpers
   def nav_link_to(title, url, active=nil)
     active = current_page.url.index(url) == 0 if active.nil?
     "<li class=\"#{"active" if active}\">" + link_to(title, url) + "</li>"
+  end
+
+  def define(term)
+    definition = data.glossary.send(term.downcase)
+    raise "Term '#{term}' not defined" unless definition
+    '<span data-toggle="tooltip" title="'+definition+'">'+term+'</span>'
+  end
+
+  def slugify(str)
+    str.strip.downcase.gsub(/(&|&amp;)/, ' and ').gsub(/[\s\/\\]/, '-').gsub(/[^\w-]/, '').gsub(/[-]{2,}/, '-').gsub(/^-|-$/, '')
   end
 end
